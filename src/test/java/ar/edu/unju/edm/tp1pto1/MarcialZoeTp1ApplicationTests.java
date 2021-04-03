3f69fb0bb3a93652ac3f293cac888a55f410963d
Zoe Marcial <Zoe Marcial@LAPTOP-QPF45C5K>
2021-04-03 16:02:22 -0300

    creacion clase provincia creacion clase inmueble

---
+++ src/test/java/ar/edu/unju/edm/tp1pto1/MarcialZoeTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm.tp1pto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcialZoeTp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
